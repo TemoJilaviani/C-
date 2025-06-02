a679623a4ca819900f1813ae36fe0af5c6a6be49
TemoJilaviani <t.jilaviani@icloud.com>
2025-06-02 19:15:02 +0400

    first commit

---
+++ homework 1.cs
@@ -0,0 +1,28 @@
+﻿string name;
+int age;
+string hobby;
+int favoriteNumber;
+
+Console.Write("Enter your name: ");
+name = Console.ReadLine();
+
+Console.Write("Enter your age: ");
+age = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("What is your favorite hobby? ");
+hobby = Console.ReadLine();
+
+Console.Write("What is your favorite number? ");
+favoriteNumber = Convert.ToInt32(Console.ReadLine());
+
+int numberSquared = favoriteNumber * favoriteNumber;
+
+
+Console.Clear();
+
+Console.WriteLine("==== Summary ====");
+Console.WriteLine($"Hello {name}!");
+Console.WriteLine($"You are {age} years old.");
+Console.WriteLine($"Your favorite hobby is {hobby}.");
+Console.WriteLine($"Your favorite number is {favoriteNumber} and its square is {numberSquared}.");
+Console.WriteLine("Thanks for answering the questions!");
---
+++ homework 2.cs
@@ -0,0 +1,26 @@
+﻿double balance;
+double depositAmount;
+double withdrawAmount;
+
+Console.Write("Enter your initial balance: ");
+balance = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter amount to deposit: ");
+depositAmount = Convert.ToDouble(Console.ReadLine());
+balance = balance + depositAmount;
+Console.WriteLine($"You deposited {depositAmount}. New balance: {balance}");
+
+Console.Write("Enter amount to withdraw: ");
+withdrawAmount = Convert.ToDouble(Console.ReadLine());
+
+if (withdrawAmount > balance)
+{
+    Console.WriteLine("Not enough balance.");
+}
+else
+{
+    balance = balance - withdrawAmount;
+    Console.WriteLine($"You withdrew {withdrawAmount}. New balance: {balance}");
+}
+
+Console.WriteLine($"Your final balance is: {balance}");
